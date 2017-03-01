3228084508f33f7e16707b3cee9d4a901f3519e2
Alejandro Fernandez Gonzalez <Alex@alejandros-macbook-pro.local>
2017-03-01 09:01:16 +0100

    Video Cam

--- SimpleVideoCam/SimpleVideoCamController.swift
+++ SimpleVideoCam/SimpleVideoCamController.swift
@@ -6,15 +6,45 @@
 //  Copyright © 2017 355 Berry Street S.L. All rights reserved.
 //
 import UIKit
+import AVFoundation
 
 class SimpleVideoCamController: UIViewController {
 
     @IBOutlet var cameraButton:UIButton!
     
+    let captureSession = AVCaptureSession()
+    var currentDevice: AVCaptureDevice?
+    var videoFIleOutput: AVCaptureMovieFileOutput?
+    var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
+    
+    var isRecording = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        captureSession.sessionPreset = AVCaptureSessionPresetHigh
+        if let device = AVCaptureDevice.defaultDevice(withDeviceType: .builtInDuoCamera, mediaType: AVMediaTypeVideo, position: .back){
+        currentDevice = device
+        } else if let device = AVCaptureDevice.defaultDevice(withDeviceType: AVCaptureDeviceType.builtInWideAngleCamera, mediaType: AVMediaTypeVideo, position: .back){
+            currentDevice = device
+        } else {
+        //Fallback on earlier versions
+
+        }
+        guard let captureDeviceInput = try? AVCaptureDeviceInput(device: currentDevice) else { return }
+        
+        videoFIleOutput = AVCaptureMovieFileOutput()
+        
+        captureSession.addInput(captureDeviceInput)
+        captureSession.addOutput(videoFIleOutput)
+        
+        cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(cameraPreviewLayer!)
+        cameraPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+        cameraPreviewLayer?.frame = view.layer.frame
+        
+        view.bringSubview(toFront: cameraButton)
+        captureSession.startRunning()
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,7 +60,13 @@ class SimpleVideoCamController: UIViewController {
     }
     
     @IBAction func capture(sender: AnyObject) {
+        
+        if !isRecording {
+            isRecording = true
+            UIView.animate(withDuration: 0.5, delay: 0.0, options: [.repeat, .autoreverse, .allowUserInteraction], animations: { self.cameraButton.transform = CGAffineTransform(scaleX: 0.5, y: 0.5) }, completion: nil)
+        }
+
+
     }
-
-
 }
+
